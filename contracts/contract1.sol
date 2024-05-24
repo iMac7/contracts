8fcbd7ddd585b03704d0a75f153f66f43d5642cc
= <macharian77@gmail.com>
2024-05-24 18:53:35 +0300

    added getbalance

--- contracts/contract1.sol
+++ contracts/contract1.sol
@@ -4,12 +4,28 @@ pragma solidity ^0.8.17;
 // Creating a contract
 contract contract1
 {
-	// Defining a function
+	address public owner;
+
+	constructor() {
+		owner = msg.sender;
+	}
+
+	modifier only_owner() {
+		require(owner == msg.sender, "not the owner!"); 
+		_;
+	}
+
 	function get_output() public pure returns (string memory){
 		return ("Hi, your contract ran successfully");
 	}
 
-  function get_sender() public view returns (address){
-    return msg.sender;
-  }
+	function get_sender() public view returns (address){
+		return msg.sender;
+	}
+
+	function get_balance() public view only_owner() returns (uint256)  {
+	  	return address(this).balance;
+	}
+
+	fallback() external payable {}
 }
